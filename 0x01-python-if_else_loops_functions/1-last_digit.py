7b2eae0f7d14ff81ad10beb3607eb377d7cccfcf
nickfff-dev <hunchographics@gmail.com>
2023-08-09 00:35:37 +0300

    fixed pystyle error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,15 +3,19 @@ import random
 number = random.randint(-10000, 10000)
 while True:
     if number % 10 == 0:
-        print('Last digit of {:d} is {:d} and is 0'.format(number, number % 10))
+        print('Last digit of {:d} is {:d} and is 0'
+              .format(number, number % 10))
         break
     elif number < 0:
-        print('Last digit of {:d} is -{:d} and is less than 6 and not 0'.format(number, abs(number) % 10))
+        print('Last digit of {:d} is -{:d} and is less than 6 and not 0'
+              .format(number, abs(number) % 10))
         break
     else:
         if number % 10 > 5:
-            print('Last digit of {:d} is {:d} and is greater than 5'.format(number, number % 10))
+            print('Last digit of {:d} is {:d} and is greater than 5'
+                  .format(number, number % 10))
             break
         else:
-            print('Last digit of {:d} is {:d} and is less than 6 and not 0'.format(number, number % 10))
+            print('Last digit of {:d} is {:d} and is less than 6 and not 0'
+                  .format(number, number % 10))
             break

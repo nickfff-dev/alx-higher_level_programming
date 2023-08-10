4a0d3dfb722bf59c67af6650eea57616cf0618d4
nickfff-dev <hunchographics@gmail.com>
2023-08-11 01:50:43 +0300

    2 rectify default use case

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,9 @@ if __name__ == "__main__":
         print('{:d} arguments:'.format(args - 1))
     elif args == 2:
         print('{0} argument:\n1: {1}'.format(args - 1, sys.argv[1]))
-    else:
+    elif args > 2:
         print('{0} arguments:'.format(args - 1))
         for i in range(1, args):
             print('{0}: {1}'.format(i, sys.argv[i]))
+    else:
+        print('{}'.format(args - 1))

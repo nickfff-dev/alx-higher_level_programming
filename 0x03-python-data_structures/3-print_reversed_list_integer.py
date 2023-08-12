999db6781a8251a2277430ba2ee2ecc4cd3eb0f2
nickfff-dev <hunchographics@gmail.com>
2023-08-12 18:19:06 +0300

    reverse order rectify

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
+    my_list.sort(reverse=True)
     for item in my_list:
         print('{:d}'.format(item))

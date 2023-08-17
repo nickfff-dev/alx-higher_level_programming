94667aeed0741da5ee621bc74915edfbf5b9b6f5
nickfff-dev <hunchographics@gmail.com>
2023-08-17 23:56:25 +0300

    switched to map and lambda

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,5 +4,5 @@ def square_matrix_simple(matrix=[]):
     if isEmpty == 0:
         return None
     else:
-        squares = [[elem ** 2 for elem in row] for row in matrix]
+        squares = list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
         return squares

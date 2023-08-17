8e5d905ef8282d571f2343297723cbb4a06bdbbc
nickfff-dev <hunchographics@gmail.com>
2023-08-18 01:28:29 +0300

    set

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 def search_replace(my_list, search, replace):
     isEmpty = len(my_list)
     if isEmpty == 0:
-        return None
+        return []
     else:
         nw_list = [replace if item == search else item for item in my_list]
         return nw_list

1feedc6b5033026bfb1e2451c2ba81be412a7fa3
nickfff-dev <hunchographics@gmail.com>
2023-08-18 01:54:11 +0300

    print error

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    if len(a_dictionary) == 0:
-        print("{}")
-    else:
+    if len(a_dictionary) != 0:
         new_lis = sorted(a_dictionary)
         ([print('{}: {}'.format(item, a_dictionary[item]))
-          for item in new_lis])
+            for item in new_lis])

591fb89d006849f10ff85a42c6fb63a5c90a66d1
nickfff-dev <hunchographics@gmail.com>
2023-08-18 01:59:55 +0300

    print

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    if len(a_dictionary) != 0:
+    if len(a_dictionary) == 0:
+        print({})
+    else:
         new_lis = sorted(a_dictionary)
-        ([print('{}: {}'.format(item, a_dictionary[item]))
-            for item in new_lis])
+        ({print('{}: {}'.format(item, a_dictionary[item]))
+          for item in new_lis})

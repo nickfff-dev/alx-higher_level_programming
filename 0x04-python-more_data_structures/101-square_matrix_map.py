64cbc19db32558327c0e44a7845acc2d70e2356d
nickfff-dev <hunchographics@gmail.com>
2023-08-21 00:30:25 +0300

    correction

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    if len(matrix) == 0:
-        return matrix
-    else:
-        sq_matrix = \
-            list(map(lambda x: list(map(lambda y: y ** 2, x)), matrix))
-        return sq_matrix
+    return list(map(lambda x: list(map(lambda y: y ** 2, x)), matrix))

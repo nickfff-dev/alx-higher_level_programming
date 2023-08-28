07be4e8e715d2d2292749b4c466d67dc762e76dc
nickfff-dev <hunchographics@gmail.com>
2023-08-29 01:21:16 +0300

    catch all exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,5 +7,7 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end='')
         except IndexError:
             return count
+        except Exception:
+            return count
     print()
     return count

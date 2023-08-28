00db0974ebe22bdb909e8bda06bac8e3e53ad7d8
nickfff-dev <hunchographics@gmail.com>
2023-08-29 02:07:29 +0300

    divide list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,13 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        except IndexError:
-            continue
-        except ValueError:
-            continue
-        except TypeError:
-            continue
-        except Exception:
+        except (ValueError, TypeError, IndexError):
             continue
     print()
     return count

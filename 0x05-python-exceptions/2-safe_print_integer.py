835d2baac67aa8a2634747ce8fcc2002d7a46f2f
nickfff-dev <hunchographics@gmail.com>
2023-08-29 01:48:13 +0300

    2 safe

---
+++ 0x05-python-exceptions/2-safe_print_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    if x == 0:
+        return count
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except ValueError:
+            continue
+        except IndexError:
+            continue
+        except Exception:
+            continue
+
+    print()
+    return count

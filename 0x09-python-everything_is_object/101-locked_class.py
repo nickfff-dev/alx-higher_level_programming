e7642b3856077250c7b230e32b4412ce4a6485dd
nickfff-dev <hunchographics@gmail.com>
2023-09-07 00:33:39 +0300

    try with slots

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""
+A class that only allows the creation of the 'first_name' attribute.
+Any attempt to create other attributes will raise an AttributeError.
+"""
+
+
 class LockedClass:
+    """
+    A class that only allows the creation of the 'first_name' attribute.
+    Any attempt to create other attributes will raise an AttributeError.
+    """
     __slots__ = ['first_name']

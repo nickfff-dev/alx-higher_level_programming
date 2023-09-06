75472392709f17587682ed899ff3c0fd2419d3b4
nickfff-dev <hunchographics@gmail.com>
2023-09-07 00:31:59 +0300

    try with slots

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,33 +1,3 @@
 #!/usr/bin/python3
 class LockedClass:
-    """
-    A class that only allows the creation of the
-    'first_name' attribute.
-    Any attempt to create other attributes will
-    raise an AttributeError.
-    """
-
-    def __init__(self):
-        """
-        Initialize the 'first_name' attribute.
-        """
-        self.__dict__['first_name'] = None
-
-    def __setattr__(self, name, value):
-        """
-        Set the value of 'first_name'.
-        Raise an AttributeError if any other attribute
-        is tried to be set.
-
-        Args:
-            name (str): The name of the attribute.
-            value: The value to be assigned to the attribute.
-
-        Raises:
-            AttributeError: If an attribute other than
-            'first_name' is tried to be set.
-        """
-        if name == 'first_name':
-            super().__setattr__(name, value)
-        else:
-            raise AttributeError(f"'LockedClass' has no attribute {name}")
+    __slots__ = ['first_name']

f40df7d1144f2823f78b903e731ce8e54ca64a2d
nickfff-dev <hunchographics@gmail.com>
2023-09-09 00:20:01 +0300

    say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""""
+
+This module contains a function that prints a name
+
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints a message with the given first name and last name.
+
+    Args:
+        first_name (str): The first name.
+        last_name (str, optional): The last name. Defaults to
+        an empty string.
+
+    Raises:
+        TypeError: If first_name or last_name is not a string.
+    """
+    if not isinstance(first_name, str) or not isinstance(last_name, str):
+        raise TypeError("first_name must be a string or last_name must be \
+                        a string")
+    print(f"My name is {first_name} {last_name}")

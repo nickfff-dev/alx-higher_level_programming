c8d7f87031a7cad5f0c80a38cc24d9dfc43b65ee
nickfff-dev <hunchographics@gmail.com>
2023-09-12 22:50:55 +0300

    file is read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,10 @@
 
 
 def read_file(filename=""):
-    """ function that reads a file """
+    """ function that reads a file
+
+        Args:
+            filename(str): a path to the file
+    """
     with open(filename, encoding='utf-8') as myfile:
         print(myfile.read())

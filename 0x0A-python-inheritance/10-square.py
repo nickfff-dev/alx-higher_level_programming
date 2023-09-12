5ea600c463a2329a2952a75357da8e273f330954
nickfff-dev <hunchographics@gmail.com>
2023-09-12 20:00:23 +0300

    square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+"""
+This module defines a class Square
+"""
+
+
+class Square(Rectangle):
+    """
+    Class Square with attributes size and methods area
+
+    Attributes:
+            size(int): the size of the square
+    """
+    def __init__(self, size):
+        """
+        initializes a Square instance
+        calls ancestor method integer_validator
+        to validate type of size
+
+        Args:
+            size(int): the size of the square
+        """
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        Method for getting the area of a square
+
+        Returns:
+            int: the area of the square
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """
+        instance method that prints user friendly format class
+        info
+        """
+        return "[" + self.__class__.__name__ + "] " + \
+            str(self.__size) + "/" + str(self.__size)

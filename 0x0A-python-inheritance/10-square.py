9460bf5206b47415a57ed3fc44c9d7f9500e4e03
nickfff-dev <hunchographics@gmail.com>
2023-09-12 20:59:18 +0300

    whats wrong

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,6 +9,5 @@ class Square(Rectangle):
     def __init__(self, size):
         """Initialize a new square
         """
-        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size

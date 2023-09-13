5c940d3ac34d650473bcbc532add755830df4f09
nickfff-dev <hunchographics@gmail.com>
2023-09-13 23:38:09 +0300

    11 studeent

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -15,10 +15,13 @@ def pascal_triangle(n):
     Returns:
         list: a list of lists
     """
-
-    nested_arr = [[1 for x in range(i+1)] if i < 2 else [1, 1]
-                  for i in range(n)]
-    for i in range(2, n):
-        for j in range(1, len(nested_arr[i])-1):
-            nested_arr[i][j] = nested_arr[i-1][j-1] + nested_arr[i-1][j]
-    return nested_arr
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        last_row = triangle[-1]
+        row += [sum(pair) for pair in zip(last_row, last_row[1:])]
+        row.append(1)
+        triangle.append(row)
+    return triangle

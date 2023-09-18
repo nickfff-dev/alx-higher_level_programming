ecf8faa5f106841a7536c6d887637a95bb5f7c2e
nickfff-dev <hunchographics@gmail.com>
2023-09-18 23:38:44 +0300

    validation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -37,6 +37,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, width):
         """ the setter/mutator for the width attribute"""
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
         self.__width = width
 
     @property
@@ -47,6 +51,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, height):
         """ the setter/mutator for the height attribute"""
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
         self.__height = height
 
     @property
@@ -57,6 +65,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, x):
         """ the setter/mutator for the x attribute"""
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
         self.__x = x
 
     @property
@@ -67,4 +79,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, y):
         """ the setter/mutator for the y attribute"""
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
         self.__y = y

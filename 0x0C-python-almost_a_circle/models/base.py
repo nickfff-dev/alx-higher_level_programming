1a4b48d8ef40be90691d4e7c6cbd4b02581db41d
nickfff-dev <hunchographics@gmail.com>
2023-09-19 21:06:58 +0300

    base correction

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,8 +18,8 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+            type(self).__nb_objects += 1
+            self.id = type(self).__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):

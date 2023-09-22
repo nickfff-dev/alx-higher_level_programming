280e3c1b6d010903c1f9a5afe85382e1a6f6e9c8
nickfff-dev <hunchographics@gmail.com>
2023-09-23 02:28:13 +0300

    all tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,39 +1,139 @@
 #!/usr/bin/python3
-"""Defines a class for the unittest of class Base"""
 import unittest
 from models.base import Base
+from models.square import Square
+import json
+import inspect
+
+"""This module Defines a class for the unittest of class Base
+"""
 
 
 class test_base(unittest.TestCase):
-    """Defines a class for the unittest of class Base"""
+    """
+        Defines a class for the unittest of class Base.
+    """
+    def test_id_none(self):
+        """
+            Test with no id
+        """
+        testnbase = Base()
+        self.assertEqual(1, testnbase.id)
+
+    def test_id(self):
+        """
+            Test with a valid id
+        """
+        testnbase = Base(50)
+        self.assertEqual(50, testnbase.id)
+
+    def test_id_zero(self):
+        """
+            Test with an id 0
+        """
+        testnbase = Base(0)
+        self.assertEqual(0, testnbase.id)
+
+    def test_id_negative(self):
+        """
+            Test with a negative id
+        """
+        testnbase = Base(-20)
+        self.assertEqual(-20, testnbase.id)
+
+    def test_id_string(self):
+        """
+            Test with an id that is not an int
+        """
+        testnbase = Base("Betty")
+        self.assertEqual("Betty", testnbase.id)
+
+    def test_id_list(self):
+        """
+            Test with an id that is not an int
+        """
+        testnbase = Base([1, 2, 3])
+        self.assertEqual([1, 2, 3], testnbase.id)
+
+    def test_id_dict(self):
+        """
+            Test with an id that is not an int
+        """
+        testnbase = Base({"id": 109})
+        self.assertEqual({"id": 109}, testnbase.id)
+
+    def test_id_tuple(self):
+        """
+            Test with an id that is not an int
+        """
+        testnbase = Base((8,))
+        self.assertEqual((8,), testnbase.id)
+
+    def test_to_json_type(self):
+        """
+            Testing the json string
+        """
+        testnsquare = Square(1)
+        json_dict = testnsquare.to_dictionary()
+        json_string = Base.to_json_string([json_dict])
+        self.assertEqual(type(json_string), str)
+
+    def test_to_json_value(self):
+        """
+            Testing the json string
+        """
+        testnsquare = Square(1, 0, 0, 609)
+        json_dict = testnsquare.to_dictionary()
+        json_string = Base.to_json_string([json_dict])
+        self.assertEqual(json.loads(json_string),
+                         [{"id": 609, "y": 0, "size": 1, "x": 0}])
+
+    def test_to_json_None(self):
+        """
+            Testing the json string
+        """
+        testnsquare = Square(1, 0, 0, 609)
+        json_dict = testnsquare.to_dictionary()
+        json_string = Base.to_json_string(None)
+        self.assertEqual(json_string, "[]")
+
+    def test_to_json_Empty(self):
+        """
+            Testing the json string
+        """
+        testnsquare = Square(1, 0, 0, 609)
+        json_dict = testnsquare.to_dictionary()
+        json_string = Base.to_json_string([])
+        self.assertEqual(json_string, "[]")
+
+
+class TestSquare(unittest.TestCase):
+    """
+    class for testing Base class" methods
+    """
 
     @classmethod
     def setUpClass(cls):
-        """This method will be called once at the
-        beginning of the test"""
-        cls.base1 = Base()
-        cls.base2 = Base()
-        cls.base3 = Base()
-        cls.base4 = Base(12)
-        cls.base5 = Base()
+        """
+        Set up class method for the doc tests
+        """
+        cls.setup = inspect.getmembers(Base, inspect.isfunction)
 
-    def test_id(self):
-        """Test if the 'id' has been set correctly"""
-        self.assertEqual(self.base1.id, 1, 'base1 id not set correctly')
-        self.assertEqual(self.base2.id, 2, 'base2 id not set correctly')
-        self.assertEqual(self.base3.id, 3, 'base3 id not set correctly')
-        self.assertEqual(self.base4.id, 12, 'base4 id not set correctly')
-        self.assertEqual(self.base5.id, 4, 'base5 id not set correctly')
+    def test_module_docstring(self):
+        """
+        Tests if module docstring documentation exist
+        """
+        self.assertTrue(len(Base.__doc__) >= 1)
 
-    def test_docstring(self):
-        """Test if docstrings are present"""
-        import models.base as mb
+    def test_class_docstring(self):
+        """
+        Tests if class docstring documentation exist
+        """
+        self.assertTrue(len(Base.__doc__) >= 1)
 
-        # Validating module docstring
-        self.assertIsNotNone(mb.__doc__)
-
-        # Validating Base class docstring
-        self.assertIsNotNone(Base.__doc__)
-
-        # Validating Base class method docstrings
-        self.assertIsNotNone(Base.__init__.__doc__)
+    def test_func_docstrings(self):
+        """
+        Tests if methods docstring documntation exist
+        """
+        for func in self.setup:
+            self.assertTrue(len(func[1].__doc__) >= 1)

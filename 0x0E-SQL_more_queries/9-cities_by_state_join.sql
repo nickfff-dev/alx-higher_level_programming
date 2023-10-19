bddf0ac3607ed5689fb6ffe271cdf8ba2c382732
nickfff-dev <hunchographics@gmail.com>
2023-10-20 00:20:08 +0300

    one select

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 -- This script lists all cities contained in the database
-SELECT cities.id, cities.name, (SELECT states.name FROM states WHERE states.id = cities.state_id) AS state_name 
+SELECT cities.id, cities.name, states.name 
 FROM cities 
+LEFT JOIN states ON cities.state_id = states.id 
 ORDER BY cities.id ASC;

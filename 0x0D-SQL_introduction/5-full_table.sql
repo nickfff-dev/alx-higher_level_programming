b70a0421cb5b82bedcdb732b357dfb7eb4e609d3
nickfff-dev <hunchographics@gmail.com>
2023-11-10 20:32:53 +0300

    table data

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- This script  that prints the full description of the table first_table from the database hbtn_0c_0
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;

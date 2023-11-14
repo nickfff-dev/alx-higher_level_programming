26fd90c44a908a83bea0f8091bc00819a5fc0af6
nickfff-dev <hunchographics@gmail.com>
2023-11-14 19:41:21 +0300

    argumnets

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 // script that prints argvs
-const myArr = process.argv;
-if (myArr.length < 3) {
+
+if (process.argv.length < 3) {
   console.log('No Argument');
-} else if (myArr.length === 3) {
-  console.log('Argument Found');
-} else {
-  console.log('Arguments Found');
+} else if (process.argv.length > 2) {
+  if (process.argv.length === 3) {
+    console.log('Argument Found');
+  } else {
+    console.log('Arguments Found');
+  }
 }

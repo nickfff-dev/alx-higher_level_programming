6b0496e69a1e412cec6f507fa6566236199128a9
nickfff-dev <hunchographics@gmail.com>
2023-11-19 22:12:25 +0300

    select

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This module defines a script that uses
+    mysqlclient to make queries to a db
+"""
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+    import os
+    db = MySQLdb.connect(password=sys.argv[2], database=sys.argv[3],
+                         user=sys.argv[1], port=3306)
+    cur = db.cursor()
+    cur.execute('SELECT id, name FROM states ORDER BY id ASC')
+    rows = cur.fetchall()
+    for col in rows:
+        print(col)

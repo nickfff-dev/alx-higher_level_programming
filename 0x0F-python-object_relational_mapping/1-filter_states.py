bf23003bb5a9939ffe9de94455511b0ae7c3516b
nickfff-dev <hunchographics@gmail.com>
2023-11-19 22:22:08 +0300

    LIKE N%

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" This module defines a script that uses
+    mysqlclient to make filter queries to a db
+"""
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+    import os
+    db = MySQLdb.connect(password=sys.argv[2], database=sys.argv[3],
+                         user=sys.argv[1], port=3306)
+    cur = db.cursor()
+    cur.execute('SELECT id, name FROM states WHERE NAME LIKE \
+                "N%" ORDER BY id ASC')
+    rows = cur.fetchall()
+    for col in rows:
+        print(col)

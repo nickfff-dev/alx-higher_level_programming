3d9d787a4d3b316810d030fffe1cb59337e54f60
nickfff-dev <hunchographics@gmail.com>
2024-01-18 20:48:16 +0300

    api

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
+
 const fs = require('fs');
+const filePath = process.argv[2];
 
-const filePath = process.argv[2]; // Get the file path from command line arguments
-
-if (!filePath) {
-  console.error('No path given');
-  process.exit(1);
-}
 fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
     console.error(err);

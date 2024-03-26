7ae7667ffa829fdde9a1dee101e3a803af6974d6
nickfff-dev <hunchographics@gmail.com>
2024-03-26 06:48:59 -0700

    errors

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
-    console.error(err);
+    console.log(err);
   } else {
     console.log(data.toString());
   }

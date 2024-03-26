a83c0b44d82d6a2e2b9cf95bbb1d0e7111512333
nickfff-dev <hunchographics@gmail.com>
2024-03-26 06:51:22 -0700

    errors writeme

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-const filePath = process.argv[2]; // Get the file path from command line arguments
-const strToWrite = process.argv[3]; // Get the string to write from command line arguments
-if (!strToWrite) {
-  console.error('No string given');
-  process.exit(1);
-}
-fs.writeFile(filePath, strToWrite, 'utf-8', (err) => {
+const file = process.argv[2];
+const content = process.argv[3];
+fs.writeFile(file, content, 'utf-8', function (err) {
   if (err) {
-    console.error(err);
+    console.log(err);
   }
 });
